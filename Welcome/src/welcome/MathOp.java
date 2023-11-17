535f3fdb1759078a5365c241789a9bb3503d530a
PURUSHOTHAM PANASA <114135235+panasanpurushotham@users.noreply.github.com>
2023-11-17 16:58:54 +0530

    Add files via upload

---
+++ Welcome/src/welcome/MathOp.java
@@ -0,0 +1,15 @@
+package welcome;
+
+import math.AddJarEx;
+
+public class MathOp {
+
+	public static void main(String[] args) {
+		AddJarEx obj= new AddJarEx();
+		System.out.println(obj.add(5, 4));
+		System.out.println(obj.sub(10, 5));
+		
+
+	}
+
+}
